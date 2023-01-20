9cfd157f76f12277ba637146e047466eb4b098b3
Aman Singh <amansingh1802308@gmail.com>
2023-01-20 13:55:25 +0530

    DAY 1

---
+++ CommandLine.java
@@ -0,0 +1,12 @@
+class CommandLine
+{
+    public static void main(String[] args) {
+        // Learning to use command line arguments in java which are passed via command line
+        // args in main function is used to take arguments 
+
+        String a = args[0];
+        String b = args[1];
+
+        System.out.println(a+" "+ b);
+    }
+}
---
+++ User.java
@@ -0,0 +1,6 @@
+class User
+{
+    public static void main(String[] args) {
+       System.out.println("learning basic java in techinal training");
+    }
+}
